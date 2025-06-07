0d03b2f5f3385d5390bccfecc9a3b13b2ed85f83
Diego-Moreira73 <diego.andrade.moreira@gmail.com>
2025-06-06 22:25:46 -0300

    Melhoria em algumas partes do código melhorando a visualização e organização, adição de uma interface interativa com o usuário, utilizando tkinter

--- projeto.py
+++ projeto.py
@@ -1,79 +1,69 @@
-def menu ():
-    opcao = input('''
-=================================================
-          PROJETO AGENDA EM PYTHON
-
-MENU: 
-          
-[1]CADASTRAR CONTATO
-[2]LISTAR CONTATO
-[3]DELETAR CONTATO
-[4]BUSCAR CONTATO PELO NOME
-[5]SAIR
-
-=================================================
-ESCOLHA UMA OPÇÃO ACIMA:
-''')
-    if opcao == '1':
-        cadastrarcontato()
-    elif opcao == '2':
-        listarcontato()
-    elif opcao == '3':
-        deletarcontato()
-    elif opcao == "4":
-        buscarcontatopelonome()
-    else:
-        sair()
+import tkinter as tk
+from tkinter import messagebox, simpledialog
 
 def cadastrarcontato():
-    idcontato = input('Escolha ID do seu contato: ')
-    nome = input('Escreva o nome do seu contato: ')
-    telefone = input('Escreva o telefone do contato: ')
-    email = input('Escreva o e-mail do contato: ')
-    try:
-        agenda = open("agenda.txt", "a")
-        dados = f'{idcontato};{nome};{telefone};{email} \n'
-        agenda.write(dados)
-        agenda.close()
-        print(f'Contato gravado com sucesso !!!')
-    except:
-        print('ERRO na gravação do contato')
+    idcontato = simpledialog.askstring ("ID", "Escolha ID do seu contato:")
+    nome = simpledialog.askstring ("Nome", "Escreva o nome do seu contato:")
+    telefone = simpledialog.askstring ("Telefone", "Escreva o telefone do contato:")
+    email = simpledialog.askstring ("E-mail", "Escreva o e-mail do contato:")
+    if idcontato and nome and telefone and email:
+        try:
+            with open("agenda.txt", "a") as agenda:
+                dados = f'{idcontato};{nome};{telefone};{email} \n'
+                agenda.write(dados)
+            messagebox.showinfo ("Sucesso", 'Contato gravado com sucesso !!!')
+        except:
+            messagebox.showinfo ("Erro", 'ERRO na gravação do contato.')
 
 def listarcontato():
-    agenda = open("agenda.txt", "r")
-    for contato in agenda: 
-        print(contato)
-    agenda.close()
+    try:
+        with open("agenda.txt", "r") as agenda:
+            contato = agenda.read()
+            messagebox.showinfo ("Lista de Contatos", contato if contato else "Nenhum contato encontrado")
+    except FileNotFoundError:
+        messagebox.showwarning("Aviso", "Agenda não existe.")
     
 def deletarcontato():
-    nomedeletado = input("Digite o nome a ser deletado: ")
-    agenda = open("agenda.txt","r")
-    aux = []
-    aux2 = []
-    for i in agenda:
-        aux.append(i)
-    for i in range(0, len(aux)):
-        if nomedeletado not in aux[i]:
-            aux2.append(aux[i])
-    agenda = open("agenda.txt", "w")
-    for i in aux2:
-        agenda.write(i)
-    print(f'Contato deletado com sucesso')
-    listarcontato()
+    nomedeletado = simpledialog.askstring ("Deletar", "Digite o nome a ser deletado:").lower()
+    try:
+        with open("agenda.txt","r") as agenda:
+            aux = []
+            aux2 = []
+            for i in agenda:
+                aux.append(i)
+            for i in range(0, len(aux)):
+                if nomedeletado not in aux[i].lower():
+                    aux2.append(aux[i])
+        with open("agenda.txt", "w") as agenda:
+            for i in aux2:
+                agenda.write(i)
+        messagebox.showinfo("Sucesso", 'Contato deletado com sucesso')
+    except FileNotFoundError:
+        messagebox.showwarning("Aviso", "Contato não encontrado")
 
 def buscarcontatopelonome():
-    nome = input(f'Digite o nome a ser procurado: ')
-    agenda = open("agenda.txt", "r")
-    for contato in agenda: 
-        if nome in contato.split(";")[1]:
-            print(contato)
-    agenda.close()
+    nome = simpledialog.askstring("Nome", 'Digite o nome a ser procurado:').upper()
+    try:
+        with open("agenda.txt", "r") as agenda:
+            for contato in agenda: 
+                if nome in contato.split(";")[1].upper():
+                    messagebox.showinfo(f"Contato", contato)
+    except FileNotFoundError:
+        messagebox.showwarning("Aviso", "Contato não encontrado")
 
-def sair():
-    print(f'Até mais.....!!!')
-    exit()
+def fechar():
+    root.destroy()
 
-def main():
-    menu()
+root = tk.Tk()
+root.title ("Agenda")
+root.geometry ("250x300")
 
-main()
+tk.Label (root, text="Menu da Agenda", font = ("Arial", 15)).pack (pady = 15)
+tk.Button (root, text="Cadastrar Contato", width= 30, command= cadastrarcontato).pack (pady = 3)
+tk.Button (root, text="Listar Contato", width= 30, command= listarcontato).pack (pady = 3)
+tk.Button (root, text="Deletar Contato", width= 30, command= deletarcontato).pack (pady = 3)
+tk.Button (root, text="Buscar Contato Pelo Nome", width= 30, command= buscarcontatopelonome).pack (pady = 3)
+tk.Button (root, text="Fechar", width= 30, command= fechar).pack (pady = 25)
+
+
+root.mainloop()
